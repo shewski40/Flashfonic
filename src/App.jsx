8d5339aee6e8b5b4218f80e8ed6e87305acffd3a
shewski40 <shewski@gmail.com>
2025-07-08 17:20:58 -0400

    Initial push of working FLashfonic frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from Flashfonic</h1>
+      <p>Your app is running!</p>
+    </div>
+  );
+};
+
+export default App;
