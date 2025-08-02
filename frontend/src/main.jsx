54fb0394965ca2b1d3eb7698bed8d3fe7d1d968c
shewski40 <shewski@gmail.com>
2025-08-02 09:07:11 -0400

    fixing build issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Or './App.css' depending on your setup
+import './App.css' // Corrected this line
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
